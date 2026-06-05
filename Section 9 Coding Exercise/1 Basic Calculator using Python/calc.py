ecc8b9dd4f026c35879786c4482106dd19275a41
entermix123 <balabanovjordan@gmail.com>
2026-06-05 11:10:56 +0300

    added simple project for python calculator

---
+++ Section 9 Coding Exercise/1 Basic Calculator using Python/calc.py
@@ -0,0 +1,58 @@
+# add function
+def add(x, y):
+    return x + y
+
+# subtract function
+def subtract(x, y):
+    return x - y
+
+# multiply function
+def multiply(x, y):
+    return x * y
+
+# divide function
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero not allowed."
+    else:
+        return x / y
+
+def calculator():
+    print("Select operation:")
+    print("1: Add")
+    print("2: Subtract")
+    print("1: Multiply")
+    print("1: Divide")
+
+    while True:
+        # Take input from the user
+        choice = input("Enter choices (1.2.3.4): ")
+
+        # Check if the input is correct
+        if choice in ['1', '2' ,'3' , '4']:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+            if choice == '1':
+                print(f"{num1} + {num2} = {add(num1 , num2)}")
+            
+            if choice == '2':
+                print(f"{num1} - {num2} = {subtract(num1 , num2)}")
+
+            if choice == '3':
+
+                print(f"{num1} * {num2} = {multiply(num1 , num2)}")
+
+            if choice == '4':
+                print(f"{num1} / {num2} = {divide(num1 , num2)}")
+
+        # Option to exit the loop
+        next_calculation = input ("Do you want to perform another calculation? (yes/no)")
+        if next_calculation.lower() != 'yes':
+            break
+
+        
+    print("Exiting Calculator. Goodbye!")
+
+# Call calculator function
+calculator()
